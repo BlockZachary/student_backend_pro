9ca3c58e9dc5131b4d24314c049858952d53797b
BlockZachary <zachary26626@gmail.com>
2025-06-10 15:40:44 +0800

    feat: Setup loguru Intercept

--- app/utils/log.py
+++ app/utils/log.py
@@ -1,5 +1,8 @@
+import logging
 import sys
 from functools import lru_cache
+from types import FrameType
+from typing import cast
 from loguru import logger
 
 
@@ -32,10 +35,45 @@ class LogHelper:
             format=formatter[0],
         )
 
+    def init_config(self):
+        LOGGER_NAMES = ("uvicorn.asgi", "uvicorn.access", "uvicorn", "fastapi", "app", "sqlalchemy.log",
+                        "sqlalchemy.engine",
+                        "sqlalchemy.engine.Engine")
+
+        # 清空跟日志记录器的处理器
+        logging.getLogger().handlers = [InterceptHandler()]
+
+        # 为指定的日志记录器添加 LoguruHandler
+        for logger_name in LOGGER_NAMES:
+            logging_logger = logging.getLogger(logger_name)
+            # 设置日志级别
+            logging_logger.setLevel(logging.INFO)
+            logging_logger.handlers = [InterceptHandler()]
+            # 防止日志传播给根日志记录器，避免重复输出
+            logging_logger.propagate = False
+
     @lru_cache
     def get_logger(self):
         # 获取日志记录器实例，使用 lru_cache 缓存结果，避免重复创建
         return self.logger
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        # 将日志记录转换为 loguru 的日志格式
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = str(record.levelno)
+
+        # 获取当前调用栈的深度
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
+            frame = cast(FrameType, frame.f_back)
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage(),
+        )
 
 
 # 创建 LogHelper 类的实例
